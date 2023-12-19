f269e185a67b475e694389f2e3504cbbcce5be77
DanaAlaskari <danaskari2003@gmail.com>
2023-12-19 16:40:33 +0300

    push

--- BloomsByD/Reviews/models.py
+++ BloomsByD/Reviews/models.py
@@ -5,7 +5,7 @@ class Review(models.Model):
     flower = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     username = models.CharField(max_length=255)
     opinion = models.TextField()
-
+##
 
     def __str__(self):
         return self.username
